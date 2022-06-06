958c34cc3104425fb95fc820d04878c9fb841bd2
ktomek418 <ktomek418@gmail.com>
2022-06-06 14:08:11 +0200

    Implement Dropping call

--- UE/Application/States/TalkingState.cpp
+++ UE/Application/States/TalkingState.cpp
@@ -5,6 +5,8 @@ namespace ue
 {
     TalkingState::TalkingState(Context &context)
     : BaseState(context, "TalkingState"){
+//        context.user.setAcceptCallback([&]{ handleCallEnded(); });
+        context.user.setRejectCallback([&]{ endCall(); });
     }
 
     void TalkingState::handleUnknownRecipientAfterCallAccepted()
@@ -20,10 +22,24 @@ namespace ue
         context.bts.sendCallDropped(from);
     }
 
+    void TalkingState::handleCallDropped(common::PhoneNumber)
+    {
+        context.timer.stopTimer();
+        handleCallEnded();
+    }
+
     void TalkingState::handleCallEnded()
     {
         context.currentCallingStatus.callingNumber.value = 0;
         context.currentCallingStatus.isOutgoingCall = false;
         context.setState<ConnectedState>();
     }
+
+    void TalkingState::endCall()
+    {
+        context.bts.sendCallDropped(context.currentCallingStatus.callingNumber);
+        context.timer.stopTimer();
+        handleCallEnded();
+    }
+
 }
--- UE/Application/States/TalkingState.hpp
+++ UE/Application/States/TalkingState.hpp
@@ -8,9 +8,11 @@ class TalkingState : public BaseState
 {
 private:
     void handleCallEnded ();
+    void endCall();
 public:
     void handleCallRequest(common::PhoneNumber from) override;
     void handleUnknownRecipientAfterCallAccepted() override;
+    void handleCallDropped(common::PhoneNumber) override;
     TalkingState(Context& context);
 
 };
